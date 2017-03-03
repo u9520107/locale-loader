97026f64efc961759a5e0c465f996233e038c236
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2017-03-03 14:41:33 +0800

    implemented xlf exporting logic

--- src/exportLocale.js
+++ src/exportLocale.js
@@ -1,6 +1,8 @@
 import fs from 'fs-promise';
 import path from 'path';
 import glob from 'glob';
+import xml from 'xml-js';
+import mkdirp from 'mkdirp-promise';
 import { parse, tokTypes } from 'babylon';
 import isLocaleFile from './isLocaleFile';
 import loaderRegExp from './loaderRegExp';
@@ -69,20 +71,33 @@ async function extractData(localeFile) {
   };
 }
 
+function compileFolderData(files) {
+  const output = {};
+  files.forEach(({ locale, data }) => {
+    Object.keys(data).forEach((key) => {
+      if (!output[key]) {
+        output[key] = {};
+      }
+      output[key][locale] = data[key];
+    });
+  });
+  return output;
+}
+
 async function getLocaleData({ folderPath, sourceLocale, supportedLocales }) {
   const localeFiles = (await fs.readdir(folderPath)).filter(isLocaleFile);
   const localeData = {
     path: folderPath,
-    files: [],
+    files: {},
   };
   await Promise.all(localeFiles.map(async (file) => {
     const locale = formatLocale(file.replace(/\.(js|json)$/i, ''));
     if (locale === sourceLocale || supportedLocales.indexOf(locale) > -1) {
-      localeData.files.push({
+      localeData.files[locale] = {
         file,
         locale,
         ...(await extractData(path.resolve(folderPath, file))),
-      });
+      };
     }
   }));
   return localeData;
@@ -110,18 +125,87 @@ async function getRawData({
   return rawData;
 }
 
+async function exportXlf({
+  rawData,
+  sourceLocale,
+  supportedLocales,
+  dest,
+}) {
+  // console.log(JSON.stringify(rawData, null, 2));
+  const xlfData = {};
+  const allLocales = supportedLocales.filter(locale => locale !== sourceLocale);
 
+  allLocales.forEach((locale) => {
+    xlfData[locale] = {
+      _declaration: {
+        _attributes: {
+          version: '1.0',
+        }
+      },
+      xliff: {
+        _attributes: {
+          version: '1.2',
+          xmlns: 'urn:oasis:names:tc:xliff:document:1.2',
+        },
+      },
+    };
+  });
 
+  rawData.forEach((folderData) => {
+    const sourceFile = folderData.files[sourceLocale];
+    if (sourceFile) {
+      const keys = Object.keys(sourceFile.data);
+      supportedLocales.forEach((locale) => {
+        if (locale !== sourceLocale) {
+          const targetFile = folderData.files[locale];
+          const fileName = (targetFile && targetFile.file) || `${locale}.js`;
+          const original = path.join(folderData.path, fileName);
+          const missingKeys = keys.filter(key => (!targetFile || !targetFile.data[key]));
 
-function compileData(rawData) {
-  console.log(JSON.stringify(rawData, null, 2));
+          if (missingKeys.length) {
+            const data = {
+              _attributes: {
+                original,
+                'source-language': sourceLocale,
+                'target-language': locale,
+                datatype: 'plaintext',
+              },
+              body: {
+                'trans-unit': missingKeys.map(key => ({
+                  _attributes: {
+                    id: `[${key}]`,
+                  },
+                  source: {
+                    _text: sourceFile.data[key],
+                  },
+                  target: {
+                    _text: sourceFile.data[key],
+                  },
+                })),
+              },
+            };
 
+            if (!xlfData[locale].xliff.file) {
+              xlfData[locale].xliff.file = [];
+            }
+            xlfData[locale].xliff.file.push(data);
+          }
+        }
+      });
+    }
+  });
+  if (!(await fs.exists(dest))) {
+    await mkdirp(dest);
+  }
+  await Promise.all(allLocales.map(async (locale) => {
+    const fileName = path.resolve(dest, `${locale}.xlf`);
+    await fs.writeFile(fileName, xml.json2xml(xlfData[locale], { compact: true, spaces: 4 }));
+  }));
 }
 
 export default async function exportLocale({
   src = './src/**/*',
-  dest = './build',
-  cwd = process.cwd(),
+  dest = './localization',
   sourceLocale = 'en-US',
   supportedLocales = ['en-GB', 'en-CA', 'fr-FR', 'fr-CA', 'de-DE'],
 } = {}) {
@@ -130,11 +214,17 @@ export default async function exportLocale({
     sourceLocale,
     supportedLocales,
   });
-  console.log(compileData({
+  await exportXlf({
     rawData,
+    dest,
     sourceLocale,
     supportedLocales,
-  }));
+  });
+  // console.log(compileData({
+  //   rawData,
+  //   sourceLocale,
+  //   supportedLocales,
+  // }));
 
 
   // const folderPath = path.dirname(file);
