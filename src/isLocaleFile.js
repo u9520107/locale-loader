821aaeb7988286c04d8cd13f6099308513958d02
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2017-05-16 17:15:05 +0800

    add support for es-419 like formats

--- src/isLocaleFile.js
+++ src/isLocaleFile.js
@@ -1,4 +1,4 @@
-const localeFileRegExp = /^[a-z]{2}((-|_)[a-z]{2})?\.(js|json)$/i;
+const localeFileRegExp = /^[a-z]{2}((-|_)[0-9a-z]{2}[0-9a-z]?)?\.(js|json)$/i;
 
 export default function (filename) {
   return localeFileRegExp.test(filename);
