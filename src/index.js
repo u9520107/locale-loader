ef1b02c286fcf3770c43d18f850bc127728e3da9
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2017-11-29 09:33:35 +0800

    add tests for transformLocaleLoader and localeLoader

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import fs from 'fs-extra';
-import generateLoaderContent from './lib/generateLoaderContent';
-import isLocaleFile from './lib/isLocaleFile';
-import isLoaderFile from './lib/isLoaderFile';
+import localeLoader from './lib/localeLoader';
 
-module.exports = function localeLoader(content) {
-  const callback = this.async();
-  if (isLoaderFile(content)) {
-    (async () => {
-      const files = (await fs.readdir(this.context)).filter(f => isLocaleFile(f));
-      callback(null, generateLoaderContent({
-        files,
-        chunk: !isLoaderFile.noChunk(content),
-      }));
-    })();
-  } else {
-    callback(null, content);
-  }
-};
+export default localeLoader;
--- src/transformLocaleLoader.js
+++ src/transformLocaleLoader.js
@@ -1,25 +1,3 @@
-import through from 'through2';
-import fs from 'fs-extra';
-import path from 'path';
-import isLocaleFile from './lib/isLocaleFile';
-import generateLoaderContent from './lib/generateLoaderContent';
-import isLoaderFile from './lib/isLoaderFile';
+import transformLocaleLoader from './lib/transformLocaleLoader';
 
-export default function transformLocaleLoader({
-  noChunk = false,
-} = {}) {
-  return through.obj(async function transform(file, enc, done) {
-    const content = file.contents.toString(enc);
-    if (isLoaderFile(content)) {
-      const folderPath = path.dirname(file.path);
-      const files = (await fs.readdir(folderPath)).filter(isLocaleFile);
-      const loader = generateLoaderContent({
-        files,
-        noChunk: noChunk || isLoaderFile.noChunk(content),
-      });
-      file.contents = new Buffer(loader, 'utf8');
-    }
-    this.push(file);
-    done();
-  });
-}
+export default transformLocaleLoader;
